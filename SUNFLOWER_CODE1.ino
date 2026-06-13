eb5d1e0046c73a567a1710a175d74bf53cb270c2
Meghana9010 <meghanamandalam@gmail.com>
2026-06-13 11:46:23 +0530

    Initial project upload

---
+++ SUNFLOWER_CODE1.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+Servo myservo;
+
+const int ldrLeft = A0;
+const int ldrRight = A1;
+const int servoPin = 9;
+
+int pos = 90;
+
+void setup()
+{
+  myservo.attach(servoPin);
+  myservo.write(pos);
+}
+
+void loop()
+{
+  int leftValue = analogRead(ldrLeft);
+  int rightValue = analogRead(ldrRight);
+
+  int diff = rightValue - leftValue;   // Changed here
+
+  if(diff > 30)
+  {
+    pos++;
+    if(pos > 180) pos = 180;
+    myservo.write(pos);
+  }
+  else if(diff < -30)
+  {
+    pos--;
+    if(pos < 0) pos = 0;
+    myservo.write(pos);
+  }
+
+  delay(20);
+}
+ 
